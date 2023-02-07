e28c453a0f1a67a5350189dd7f054401b0534ebd
Vinicius Gonzalez <vinix.fatos@hotmail.com>
2023-02-07 17:26:57 -0300

    Added classes & requests

---
+++ BuberBreakfast/Controllers/BreakfastController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using BuberBreakfast.Contracts.Breakfast;
+
+namespace BuberBreakfast.Controllers;
+
+[ApiController]
+public class BreakfastController : ControllerBase
+{
+    [HttpPost("/breakfasts")]
+    public IActionResult CreateBreakfast(CreateBreakfastRequest request)
+    {
+        return Ok(request);
+    }
+
+    [HttpGet("/breakfasts/{id:guid}")]
+    public IActionResult GetBreakfast(Guid id)
+    {
+        return Ok(id);
+    }
+
+    [HttpPut("/breakfasts/{id:guid}")]
+    public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request)
+    {
+        return Ok(request);
+    }
+
+    [HttpDelete("/breakfasts/{id:guid}")]
+    public IActionResult DeleteBreakfast(Guid id)
+    {
+        return Ok(id);
+    }
+
+}
